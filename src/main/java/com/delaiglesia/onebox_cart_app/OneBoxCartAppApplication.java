f565e4ba761745150abaf055d7a4edd86cb7198d
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2023-08-11 12:01:39 +0200

    Initial commit

---
+++ src/main/java/com/delaiglesia/onebox_cart_app/OneBoxCartAppApplication.java
@@ -0,0 +1,13 @@
+package com.delaiglesia.onebox_cart_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneBoxCartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneBoxCartAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delaiglesia/onebox_cart_app/OneBoxCartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delaiglesia.onebox_cart_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneBoxCartAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
